96aa2e411de27a4854130c454e805e8504ff5813
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-06-07 18:42:45 +0200

    Modification du projet initialisé via npx create-react-app pour ajout redux react-redux, creation users reducer et root reducer et affichage texte simple via app

---
+++ src/reducers/reducer_users.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { id: 1, name: 'Sarah Croche', role: 'ROOT', active: 'NON' }, 
+    { id: 2, name: 'Eléonore Labanquise', role: 'USER', active: 'OUI' }, 
+    { id: 3, name: 'Emma Karena', role: 'USER', active: 'NON' }, 
+    { id: 4, name: 'Jean Bonnot', role: 'ADMIN', active: 'OUI' } 
+  ]
+}
